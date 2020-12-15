3596ea8e71cbc205f38635c498dc5580cdb90c10
atsuki225 <atsuki443@gmail.com>
2020-12-15 13:58:27 +0900

    create Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home:Scaffold(
         appBar: AppBar(
-          title: Text("YouTubeアプリ"),
+          centerTitle: false,
+          leading: Icon(Icons.videocam),
+          title: Text(
+              "ノダアツのFlutter学習"
+          ),
+          actions: <Widget>[
+            SizedBox(
+              width: 44,
+              child: FlatButton(
+                child: Icon(Icons.search),
+                onPressed: (){
+                  //TODO
+                },
+              ),
+            ),
+            SizedBox(
+              width: 44,
+              child: FlatButton(
+                child: Icon(Icons.more_vert),
+                onPressed: (){
+                  //TODO
+                },
+              ),
+            ),
+          ],
         ),
         body: Container(),
       ),
