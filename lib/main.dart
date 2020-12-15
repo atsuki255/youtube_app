f45a1fd8711ca6e45bb26b0a1d5850a9ad62b3d1
atsuki225 <atsuki443@gmail.com>
2020-12-15 15:11:44 +0900

    add channel page

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           centerTitle: false,
           leading: Icon(Icons.videocam),
-          title: Text(
+          title: const Text(
               "ノダアツのFlutter学習"
           ),
           actions: <Widget>[
             SizedBox(
-              width: 44,
+              width: 40,
               child: FlatButton(
                 child: Icon(Icons.search),
                 onPressed: (){
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
             SizedBox(
-              width: 44,
+              width: 40,
               child: FlatButton(
                 child: Icon(Icons.more_vert),
                 onPressed: (){
@@ -39,7 +39,47 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: Container(),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Image.network(
+                          "https://yt3.ggpht.com/ytc/AAUvwnhqxIOAZQ5sa7VtGMUpY3lmRO8tMHDidWx0oqkr=s176-c-k-c0x00ffffff-no-rj"
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        const Text(
+                            "ノダアツのFlutter学習"
+                        ),
+                        FlatButton(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.video_call,
+                                color: Colors.red,
+                              ),
+                              Text("登録する"),
+                            ],
+                          ),
+                          onPressed: (){
+                            //TODO
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
